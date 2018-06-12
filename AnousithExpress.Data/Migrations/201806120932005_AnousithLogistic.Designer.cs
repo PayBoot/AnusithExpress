9dee15519c308e50ffe2660333feb7e993d7b640
boot vong <Bootvong@hotmail.com>
2018-06-12 16:39:10 +0700

    abit change

--- AnousithExpress.Data/Migrations/201806120932005_AnousithLogistic.Designer.cs
+++ AnousithExpress.Data/Migrations/201806120932005_AnousithLogistic.Designer.cs
@@ -7,13 +7,13 @@ namespace AnousithExpress.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class AnousithLogistic : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AnousithLogistic));
         
         string IMigrationMetadata.Id
         {
-            get { return "201806111638003_initial"; }
+            get { return "201806120932005_AnousithLogistic"; }
         }
         
         string IMigrationMetadata.Source
