736de0b2681356481a245ada011b40073698cb58
Aphilaphole Sihalath <aphilaphole.sihalath@gmail.com>
2018-06-13 17:42:07 +0700

    view item

--- AnusithExpress.web.mvc/Controllers/AdminController.cs
+++ AnusithExpress.web.mvc/Controllers/AdminController.cs
@@ -1,10 +1,51 @@
-﻿using System.Web.Mvc;
+﻿using AnousithExpress.Data.SingleViewModels;
+using System.Web.Mvc;
 
 namespace AnusithExpress.web.mvc.Controllers
 {
     public class AdminController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateUpdateItem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUpdateItem(ItemSingleModel model)
+        {
+            return View();
+        }
+
+        public ActionResult AllItem()
+        {
+            return View();
+        }
+
+
+        public ActionResult Customer()
+        {
+            //GridView
+            return View();
+        }
+
+        public ActionResult Customer_Items()
+        {
+            //ListView
+            return View();
+        }
+        public ActionResult PrintBarCode()
+        {
+            return View();
+        }
+        public ActionResult BarCodeRegister()
+        {
+            return View();
+        }
+        public ActionResult Consolidate()
         {
             return View();
         }
