7257d9384370c3b8fa9b64fd346e56eff8fd243c
boot vong <Bootvong@hotmail.com>
2018-06-14 14:39:35 +0700

    update

--- AnusithExpress.web.mvc/Global.asax.cs
+++ AnusithExpress.web.mvc/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ClothesStore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace AnusithExpress.web.mvc
@@ -13,6 +15,7 @@ namespace AnusithExpress.web.mvc
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
